1dfadb9307d8eb41509bc2dec633aa89a26bff8c
Dave <summerwaif643@gmail.com>
2022-10-09 17:51:37 +0200

    working backend

--- node_server/server.js
+++ node_server/server.js
@@ -2,13 +2,22 @@ const express = require('express');
 var mysql = require('mysql');
 const app = express();
 const cors = require('cors');
-const PORT = 3000;
+const bodyParser = require("body-parser");
 
+const PORT = 3000;
 const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+    origin: 'http://localhost:4200', 
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+    methods: 'POST,GET,PUT,OPTIONS,DELETE'
 };
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -19,27 +28,22 @@ var con = mysql.createConnection({
   
   con.connect(function(err) {
     if (err) throw err;
-
     console.log("Successfully connected to database.");
   
 });
 
 app.post('/login', function(req, res, next){
     // post and wait for changes on login component from angular 
-    var data = req.body.params;
-    console.log(data);
+    let username = req.body.username;
+    let password = req.body.password;
 
-    if (data){ 
-        res.sendStatus(200);
-    }
+    console.log(username, password);
 
 });
 
 app.get('/login', function(req, res, next){
     // write later
 });
-
-app.use(cors(corsOptions));
 
 // Last thing to appear on page
 app.listen(PORT, () => { 
